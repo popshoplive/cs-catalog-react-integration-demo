7ef8f23330ecf4d723f2150ab050842c2d4ed87a
pslandyl <andy.l@popshop.live>
2023-08-14 00:15:43 +0800

    feat: dead simple cs catalog integration demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useRef } from 'react';
+import { mountCSCatalog } from '@commentsold/cs-catalog';
 import './App.css';
 
 function App() {
+  const containerElRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (!containerElRef.current) return;
+    mountCSCatalog(containerElRef.current, {
+      apiUrl: 'https://openapi.commentsoldhotel.com',
+      baseUrl: '/catalog',
+      shopId: '14',
+      isShopOwner: true,
+      isSuperAdmin: true,
+      shopOwnerInfo: {
+        name: 'jim bob',
+        streetAddress: '123 w test st',
+        city: 'dayton',
+        state: 'oh',
+        zip: '45459',
+        countryCode: '1',
+        email: 'seth.white@commentsold.com',
+        phoneNumber: '513-285-3365',
+      },
+      jwtToken: 'someToken',
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" />
   );
 }
 
