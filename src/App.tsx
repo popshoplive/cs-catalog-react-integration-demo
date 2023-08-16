dcfeb1509c91ba14f50100fdfe1e35e9f96e8738
pslandyl <andy.l@popshop.live>
2023-08-17 02:41:45 +0800

    feat: update mountCSCatalog args

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from 'react';
 import { mountCSCatalog } from '@commentsold/cs-catalog';
+import "@commentsold/cs-catalog/css";
 import './App.css';
 
 function App() {
@@ -7,22 +8,22 @@ function App() {
   useEffect(() => {
     if (!containerElRef.current) return;
     mountCSCatalog(containerElRef.current, {
-      apiUrl: 'https://openapi.commentsoldhotel.com',
-      baseUrl: '/catalog',
-      shopId: '14',
-      isShopOwner: true,
-      isSuperAdmin: true,
-      shopOwnerInfo: {
-        name: 'jim bob',
-        streetAddress: '123 w test st',
-        city: 'dayton',
-        state: 'oh',
-        zip: '45459',
-        countryCode: '1',
-        email: 'seth.white@commentsold.com',
-        phoneNumber: '513-285-3365',
+      "apiUrl": "https://openapi.commentsoldhotel.com/v1",
+      "baseUrl": "/cs-inventory",
+      "shopId": "2nysxf2saeoppjv3bheelzrjm2j2",
+      "isShopOwner": true,
+      "isSuperAdmin": true,
+      "shopOwnerInfo": {
+        "name": "james0607 guo",
+        "streetAddress": "2309 sparrow",
+        "city": "forney",
+        "state": "TX",
+        "zip": "75126",
+        "countryCode": "US",
+        "email": "guoya0911@gmail.com",
+        "phoneNumber": "2763252467"
       },
-      jwtToken: 'someToken',
+      "jwtToken": "K-0Nxeb_wW-IVa-tA7jgdKwE-xqHymbuIF_dUahrPrJyZjk80Zo5o1haRRRU9KXu7M9s19UshBQnnGTirf0M7ydewkwV-CiDCXHSVLJWYXaC3Dq26KxRF7MCjweraPJp2MYTGsgQzmpc_Hibb4FHNObNG5Wx5KzRhuTxLYOwC0SeblVVlE4iWA6riMIP77AfYHidg8V_tC22T4IA88eWApKHMCrTtUK3rpO_IgtzWqpWnFPDd1bwx95wJbGNd1djK9Dx-QgWJ86fd2NPdBk_S7NMV1-iwb5EksYGmxNrGFzj7Tr7dkey0fnE6-b8dJGFblI2PIH6aIz0Lk_rRf1S9A"
     });
   }, []);
   return (
